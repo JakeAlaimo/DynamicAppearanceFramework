4b376ffa2e95d53d751a2ab05037b8fde5096d8b
JakeAlaimo <jacob.alaimo@gmail.com>
2021-10-16 15:15:52 -0400

    Amend test project setup

---
+++ src/SharedFramework.Test/SharedFramework.Test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SharedFrameworkTest
+{
+	TEST_CLASS(SharedFrameworkTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
